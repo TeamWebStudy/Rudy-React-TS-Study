44db7c19c78d90f601cf5c7eafd9d7dbd7524554
Rudy-009 <rofxnaos@gmail.com>
2024-09-07 12:42:01 +0900

    week01 : types basics

---
+++ types.ts
@@ -0,0 +1,8 @@
+// 1. Basic Types
+// string, number, boolean, array, undefined
+
+const joe: string = "Joe"
+const age: number = 12
+const light: boolean = true
+const numbers: number[] = [1,2,3,4]
+
