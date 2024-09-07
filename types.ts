00fb5514fc0445f80e548e0ba31a6c81c99075d8
Rudy-009 <rofxnaos@gmail.com>
2024-09-07 13:32:08 +0900

    TS: ~5. read-only

--- types.ts
+++ types.ts
@@ -6,3 +6,46 @@ const age: number = 12
 const light: boolean = true
 const numbers: number[] = [1,2,3,4]
 
+// 2. Type Alias & Optional
+type Name = string
+type Age = number
+type Player = {
+    name: Name,
+    age?:  Age // Optional : value or undefined
+}
+
+// 3. Function
+function playerMakerBefore(name: string) {
+    return {
+        name:name
+    }
+}
+let jen = playerMakerBefore("Jen")
+//jen.age =  age Not Found
+
+function playerMaker(name: string) : Player {
+    return {
+        name
+    }
+}
+
+// 4. Tuple
+const tuple: [string, number, boolean] = ["name", 23, false]
+tuple[0] = "new name"
+tuple[2] = true
+
+// 5. Read-only
+
+// var unchangable: readonly number = 10; //'readonly' type modifier is only permitted on array and tuple literal
+const readonlyArray: readonly number[] = [1,2,3,4]
+// readonlyArray.push(5); // Error : it is a read-only property
+const readonlyTuple: readonly [string, number, boolean] = ["name", 23, false]
+// readonlyTuple[0] = "other name" // Error : it is a read-only property
+
+type Car = {
+    readonly name: Name
+    engine: number
+}
+
+// 6. undefined / null / any
+
